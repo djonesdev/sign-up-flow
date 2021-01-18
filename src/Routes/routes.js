2f81969d1fddd760e57e868a0f33192e1c4c2ad3
Dom Jones <domajones@outlook.com>
2021-01-18 03:31:32 +0000

    remove protected routes

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -7,7 +7,7 @@ import ConfirmationContainer from '../pages/Confirmation/Confirmation.page'
 import { HomepageContainer } from '../pages/Homepage/Homepage.page'
 import NotFound from '../pages/NotFound/NotFound'
 import SignUpError from '../pages/SignUpError/SignUpError'
-import PrivateRoute from './PrivateRoute'
+// import PrivateRoute from './PrivateRoute'
 import AuthRedirect from '../pages/AuthRedirect/AuthRedirect'
 
 export const Routing = ({ store, history }) => (
@@ -16,8 +16,11 @@ export const Routing = ({ store, history }) => (
       <div>   
         <Switch>
           <Route exact path="/" component={SignUpContainer} />
-          <PrivateRoute exact path="/Confirmation" component={ConfirmationContainer} />
-          <PrivateRoute exact path="/Homepage" component={HomepageContainer} />
+          {/* I wanted to implement protected routes, but unfortunately the authenticate endpoint wasn't playing nicely for me */}
+          {/* <PrivateRoute exact path="/Confirmation" component={ConfirmationContainer} />
+          <PrivateRoute exact path="/Homepage" component={HomepageContainer} /> */}
+          <Route exact path="/Confirmation" component={ConfirmationContainer} />
+          <Route exact path="/Homepage" component={HomepageContainer} />
           <Route exact path="/Auth-Redirect" component={AuthRedirect} />
           <Route exact path="/SignUpError" component={SignUpError} />
           <Route component={NotFound} />
